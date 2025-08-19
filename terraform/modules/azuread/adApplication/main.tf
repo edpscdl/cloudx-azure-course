5a46a8fd227c450ca84db15e3126e7da8f9252bc
Heorhi Utseuski <Heorhi.Utseuski@mckesson.com>
2025-08-19 12:59:50 +0200

    commit22

--- terraform/modules/azuread/adApplication/main.tf
+++ terraform/modules/azuread/adApplication/main.tf
@@ -1,8 +1,24 @@
 data "azuread_client_config" "current" {}
+data "azuread_application_published_app_ids" "well_known" {}
+
+data "azuread_service_principal" "microsoftGraph" {
+  client_id = data.azuread_application_published_app_ids.well_known.result["MicrosoftGraph"]
+}
 
 resource "azuread_application_registration" "applicationRegistration" {
   display_name       = var.applicationRegistrationDisplayName
   sign_in_audience   = "AzureADMyOrg"
+}
+
+resource "azuread_application_api_access" "graph_permissions" {
+  application_id = azuread_application_registration.applicationRegistration.id
+
+  api_client_id  = data.azuread_service_principal.microsoftGraph.client_id
+
+  scope_ids = [
+    data.azuread_service_principal.microsoftGraph.oauth2_permission_scope_ids["openid"],
+    data.azuread_service_principal.microsoftGraph.oauth2_permission_scope_ids["offline_access"]
+  ]
 }
 
 resource "azuread_application_owner" "applicationOwner" {
@@ -19,4 +35,14 @@ resource "azuread_service_principal" "servicePrincipal" {
 resource "azuread_application_password" "applicationPassword" {
   application_id = azuread_application_registration.applicationRegistration.id
   display_name   = var.applicationPasswordDisplayName
+}
+
+resource "azuread_service_principal_delegated_permission_grant" "servicePrincipalDelegatedPermissionGrant" {
+  service_principal_object_id = azuread_service_principal.servicePrincipal.object_id
+  resource_service_principal_object_id = data.azuread_service_principal.microsoftGraph.object_id
+
+  claim_values = [
+    data.azuread_service_principal.microsoftGraph.oauth2_permission_scope_ids["openid"],
+    data.azuread_service_principal.microsoftGraph.oauth2_permission_scope_ids["offline_access"]
+  ]
 }
